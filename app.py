49ca8cb0decb32dcab41cc141bda999c14d6dbed
ConnorHehnFairfield <117684841+ConnorHehnFairfield@users.noreply.github.com>
2023-12-06 19:52:28 +0000

    final

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 import flask
+import numpy as np
+import pandas as pd
+import pickle as pk
 from flask import Flask, render_template, request
 
 app=Flask(__name__)
 
 def init():
+    global data_encoder, random_forest_model
+
     print("initializing... ") 
+    data_encoder = pk.load(open("models/label_encoder_map.pkl", "rb"))
+    random_forest_model = pk.load(open("models/model.pkl", "rb")) 
+    print("initialized") 
   
 @app.route('/')
 def index():
@@ -12,12 +20,36 @@ def index():
 
 @app.route('/predict', methods = ['POST'])
 def predict():
-    tagValuePairs = request.form.to_dict()
-    print(tagValuePairs)
+    # Get values from html form
+    to_predict_values = request.form.to_dict()
+    to_predict_values = list(to_predict_values.values())
+    to_predict = np.array(to_predict_values).reshape(1,8)
 
-    return render_template('result.html', rc="hello from predict..")
+    # Conver values to dataframe
+    df = pd.DataFrame(to_predict, columns = ['Age','Gender','Education Level','Job Title','Years of Experience', 'Country', 'Race', 'Senior']) 
+    
+    # Replace values using encoder
+    df.replace(data_encoder, inplace=True)
+
+    # Rearrange order of columns
+    new_column_order = ['Age','Education Level','Years of Experience','Senior','Gender','Job Title','Country','Race']
+    df_reordered = df[new_column_order]
+
+    # Need to rename columns
+    column_name_mapping = {
+    'Country': 'Country_Encoded',
+    'Gender': 'Gender_Encoded',
+    'Job Title': 'Job Title_Encoded',
+    'Race': 'Race_Encoded'
+    }
+    df_reordered.rename(columns=column_name_mapping, inplace=True)
+
+    # Predict salary using model
+    y_pred = random_forest_model.predict(df_reordered)
+
+    # Return value to the result.html file
+    return render_template('result.html', prediction=y_pred)
  
-
 
 if __name__ == '__main__':
     init()
